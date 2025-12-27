830002efa0fe0932a5fd7dfdea740019146a4475
Vladislav Vasilev <vvasilev1@uclan.ac.uk>
2025-12-27 20:19:39 +0300

    Project Setup & Required Scene Layout

--- Assessment1/Assessment1.cpp
+++ Assessment1/Assessment1.cpp
@@ -13,6 +13,29 @@ void main()
 	myEngine->AddMediaFolder( "C:\\Users\\adm\\TL-Engine\\Media" );
 
 	/**** Set up your scene here ****/
+	IMesh* waterMesh = myEngine->LoadMesh("water.x");
+	IModel* water = waterMesh->CreateModel(0.0f, -5.0f, 0.0f);
+
+	IMesh* islandMesh = myEngine->LoadMesh("island.x");
+	IModel* island = islandMesh->CreateModel(0.0f, -5.0f, 0.0f);
+
+
+	IMesh* sphereMesh = myEngine->LoadMesh("spheremesh.x");
+	IModel* playerSphere = sphereMesh->CreateModel(0.0f, 10.0f, 0.0f);
+
+
+	IMesh* cubeMesh = myEngine->LoadMesh("minicube.x");
+
+	IModel* cube1 = cubeMesh->CreateModel(-80.0f, 2.5f, 80.0f);
+	IModel* cube2 = cubeMesh->CreateModel(80.0f, 2.5f, 80.0f);
+	IModel* cube3 = cubeMesh->CreateModel(80.0f, 2.5f, -80.0f);
+	IModel* cube4 = cubeMesh->CreateModel(-80.0f, 2.5f, -80.0f);
+
+	IMesh* skyMesh = myEngine->LoadMesh("sky.x");
+	IModel* sky = skyMesh->CreateModel(0.0f, -960.0f, 0.0f);
+
+	ICamera* camera = myEngine->CreateCamera(kManual, 0.0f, 200.0f, 0.0f);
+	camera->RotateX(90.0f);
 
 
 	// The main game loop, repeat until engine is stopped
